3650295d9c31976d0da4b6d9995b0c51713ba3aa
Panos Xynos <panos@onlyascii.io>
2020-04-09 17:39:33 +0100

    use nodejs version 12.16.0

--- ci/fetch_blobs.sh
+++ ci/fetch_blobs.sh
@@ -8,3 +8,6 @@ mkdir -p blobs
 
 curl -L -o "blobs/nodenv-${NODENV_VERSION}.tar.gz" "$NODENV_URL"
 curl -L -o "blobs/node-build-${NODE_BUILD_VERSION}.tar.gz" "$NODE_BUILD_URL"
+
+bosh add-blob "blobs/${NODENV_FILE}" "$NODENV_FILE"
+bosh add-blob "blobs/${NODE_BUILD_FILE}" "$NODE_BUILD_FILE"
